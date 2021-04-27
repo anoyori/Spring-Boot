9d11c49d0a2baf7bc05750c43bfb3f7d7f0e1502
Amanda <anoyori@gmail.com>
2021-04-27 16:05:20 -0300

    Iniciando um blog pessoal

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,64 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "postagem")
+
+public class Postagem {
+	
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //transform in primary key
+	
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100) //constructor, include limits of character
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 5, max = 500) 
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	//getter and setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
